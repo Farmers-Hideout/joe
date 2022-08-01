f762d1e2e9679d24ca4a2280f316b8131dc889c0
byggmesterPRO <einar.k.holt@gmail.com>
2022-08-01 12:43:32 +0200

    Added couple of files, added database in main.py

---
+++ TODO.md
@@ -0,0 +1,9 @@
+#Farmer Joe's Todo List
+This is farmer Joe's todo list, this is where you can find what has to be done
+
+##cogs/functions
+This is the folder functions holding economy and fun
+###economy.py
+- [] Create the whole economy system
+###fun.py
+ - [] Brainstorm
--- main.py
+++ main.py
@@ -1,10 +1,37 @@
 #Import
 import discord
 import asyncio
+import json
+import os
+import asyncio
+import asyncpg
 
 #From Imports
 from discord.ext import commands
+from dotenv import load_dotenv
 
+#Opening and loading config json variables with function for soft coding optimalization ;)
+def readJson(file_path):
+    with open(file_path, "r") as f:
+        data = json.load(f)
+    return data
+def writeJson(file_path, new_data):
+    with open(file_path, "w") as f:
+        data = json.dump(new_data,f)
+    return data
+
+#Defining
+config_variables = readJson("lib/json/config.json")
+debug_guild = config_variables['debug_guild']
+cogs = config_variables['cogs']
+
+#Defining hidden .env with secret variables like token through .env
+dotenv_path = os.path.join(os.path.dirname(__file__), '.env')
+load_dotenv(dotenv_path)
+
+TOKEN = os.environ.get("TOKEN")
+HOST = os.environ.get("HOST")
+DB_PW = os.environ.get("DB_PW")
 
 #Intents
 intents = discord.Intents.all()
@@ -12,9 +39,13 @@ intents.members = True
 intents.guilds = True
 intents.messages = True
 
+#Initializing a database pool into a bot class variable
+async def create_db_pool():
+    bot.db = await asyncpg.create_pool(database="joe", user="einar", host=HOST, password=DB_PW)
 
-bot = commands.Bot(description='Farmer Joe', help_command=None, intents=intents,debug_guild=936761578748002334,auto_sync_commands=True)
-cogs = []
+#Defines the bot
+bot = commands.Bot(description='Farmer Joe', help_command=None, intents=intents,debug_guild=debug_guild,auto_sync_commands=True)
+
 for f in cogs:
     try:
         bot.load_extension(f'lib.cogs.{f}')
@@ -29,5 +60,7 @@ for f in cogs:
 async def main():
     print("main")
 if __name__ == "__main__":
+    db_loop = asyncio.get_event_loop()
+    db_loop.run_until_complete(create_db_pool())
     asyncio.run(main())
     bot.run("TOKEN")
